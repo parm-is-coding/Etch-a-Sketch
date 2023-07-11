8921523d7c8f31316fd6b9a3ab9121675fbf8ce8
parm-is-coding <pardhi1400@gmail.com>
2023-07-11 13:18:44 -0700

    edit create row fn to add 16 columns to each row

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 console.log("hello World");
 const container = document.querySelector("#container");
 let grid;
+function createRow(){
+    const row = document.createElement("div");
+    row.classList.add('row');
+    row.textContent = "0";
+    for(i = 0; i < 16;i++){
+        const column = document.createElement('div');
+        column.classList.add('column');
+        column.textContent = "1"; 
+        row.appendChild(column);
+    }
+    container.appendChild(row); 
+
+}
+createRow();
+
 function createGrid(num){
     
     
